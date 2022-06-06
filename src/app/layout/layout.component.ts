07878f7362b517d412d51f65a00e2bad72c6be19
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-06-06 13:18:08 -0400

    Implementado evento Window:Resize

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+
+  @HostListener('window:resize', ['$event']) onResize(event: any) {
+    console.log(event.target.innerWidth);
+  }
 
   constructor() { }
 
